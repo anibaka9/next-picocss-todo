66ac5dfca10ebd0dc764cca5cc1946838cb89841
Alexey Budkin <anibaka9@gmail.com>
2023-05-29 00:39:24 +0600

    fixed todo element

--- src/app/Todo.tsx
+++ src/app/Todo.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { TodoShape } from "@/hooks/useTodos";
+import Link from "next/link";
 import { useCallback, useState } from "react";
 
 interface TodoProps {
@@ -10,56 +11,12 @@ interface TodoProps {
   readonly clearTodo: (id: string) => void;
 }
 
-export default function Todo({
-  todo,
-  toggleIsDone,
-  updateTodo,
-  clearTodo,
-}: TodoProps) {
-  const [editValue, setEditValue] = useState<string>(todo.text);
-  const [isEdit, setIsEdit] = useState<boolean>(false);
-
-  const editTodo = useCallback(
-    (event?: React.MouseEvent<HTMLInputElement>) => {
-      const target = event ? (event.target as HTMLInputElement) : undefined;
-      if (target) {
-        target.setSelectionRange(target.value.length, target.value.length);
-      }
-      setIsEdit(true);
-      setEditValue(todo.text);
-    },
-    [todo.text]
-  );
-
-  const endEdit = useCallback(() => {
-    if (isEdit) {
-      updateTodo(todo.id, editValue);
-
-      setIsEdit(false);
-    }
-  }, [editValue, isEdit, todo.id, updateTodo]);
-
-  const onSubmitEditingTodo = useCallback(
-    (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if (event.key === "Enter") {
-        endEdit();
-      }
-    },
-    [endEdit]
-  );
-
+export default function Todo({ todo, toggleIsDone, clearTodo }: TodoProps) {
   const onCheckboxChange = useCallback(
     (id: string) => () => {
       toggleIsDone(id);
     },
     [toggleIsDone]
-  );
-
-  const onInputChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setEditValue(event.target.value);
-    },
-    []
   );
 
   const onClearTodoClick = useCallback(
@@ -69,35 +26,33 @@ export default function Todo({
     [clearTodo]
   );
 
-  const onEditButtonClick = useCallback(() => {
-    editTodo();
-  }, [editTodo]);
-
   return (
-    <div className="grid">
-      <input
-        type="checkbox"
-        checked={todo.isDone}
-        onChange={onCheckboxChange(todo.id)}
-      ></input>
-      <input
-        value={editValue}
-        readOnly={!isEdit}
-        onBlur={endEdit}
-        onChange={onInputChange}
-        onDoubleClick={editTodo}
-        onKeyDown={onSubmitEditingTodo}
-      />
-      <button onClick={onEditButtonClick} type="button" className="outline">
-        ✎
-      </button>
-      <button
-        onClick={onClearTodoClick(todo.id)}
-        type="button"
-        className="outline"
-      >
-        ×
-      </button>{" "}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: "1rem",
+      }}
+    >
+      <label htmlFor="check">
+        <input
+          type="checkbox"
+          checked={todo.isDone}
+          onChange={onCheckboxChange(todo.id)}
+        ></input>
+        {todo.isDone ? <s>{todo.text}</s> : <strong>{todo.text}</strong>}
+      </label>
+      <div>
+        <button
+          onClick={onClearTodoClick(todo.id)}
+          type="button"
+          className="secondary outline"
+          role="button"
+        >
+          ×
+        </button>
+      </div>
     </div>
   );
 }
